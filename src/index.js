66f57fac82665598dc3a92172d8a24df6acb917e
ORiveraJr84 <oriverajr84@gmail.com>
2022-02-11 10:09:34 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const { ApolloServer } = require("apollo-server");
+const { PrismaClient } = require("@prisma/client");
+const fs = require("fs");
+const path = require("path");
+
+const prisma = new PrismaClient();
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
+  },
+
+  Mutation: {
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
+
+      return newLink;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  resolvers,
+  context: {
+    prisma,
+  },
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
