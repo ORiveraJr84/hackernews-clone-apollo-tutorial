78a81ea5d28734da2972bce418f7f2f15ce496b7
ORiveraJr84 <oriverajr84@gmail.com>
2022-02-16 17:34:32 -0500

    Tutorial complete

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,23 @@
-function feed(parent, args, context) {
-  return context.prisma.link.findMany();
+async function feed(parent, args, context) {
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {};
+
+  const links = context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  });
+
+  const count = await context.prisma.link.count({ where });
+
+  return { links, count };
 }
 
 module.exports = {
